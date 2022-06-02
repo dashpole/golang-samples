d76a26aaacad9d79cc58cb2c11d3b5ac675c9212
David Ashpole <dashpole@google.com>
2022-06-02 21:08:17 +0000

    make cloud functions send traces to cloud trace using opentelemetry

--- functions/helloworld/go.mod
+++ functions/helloworld/go.mod
@@ -2,4 +2,12 @@ module github.com/GoogleCloudPlatform/golang-samples/functions/helloworld
 
 go 1.16
 
-require cloud.google.com/go/functions v1.0.0
+require (
+	cloud.google.com/go/functions v1.0.0
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go v0.31.0
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace v1.7.0
+	go.opentelemetry.io/contrib/detectors/gcp v1.7.1-0.20220601202618-b7910afac6ff
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.32.1-0.20220601202618-b7910afac6ff // indirect
+	go.opentelemetry.io/otel v1.7.0
+	go.opentelemetry.io/otel/sdk v1.7.0
+)
--- functions/helloworld/hello_world.go
+++ functions/helloworld/hello_world.go
@@ -18,13 +18,82 @@
 package helloworld
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	"go.opentelemetry.io/contrib/detectors/gcp"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+
+	cloudtrace "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
+	gcppropagator "github.com/GoogleCloudPlatform/opentelemetry-operations-go/propagator"
 )
 
 // HelloGet is an HTTP Cloud Function.
 func HelloGet(w http.ResponseWriter, r *http.Request) {
+	tp, err := initTracer()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+	otelHandler := otelhttp.NewHandler(http.HandlerFunc(helloGet), "Hello")
+	otelHandler.ServeHTTP(w, r)
+}
+
+func helloGet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, World!")
+}
+
+func initTracer() (*sdktrace.TracerProvider, error) {
+	// Create Google Cloud Trace exporter to be able to retrieve
+	// the collected spans.
+	exporter, err := cloudtrace.New()
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := resource.New(context.Background(),
+		// Use the GCP resource detector!
+		resource.WithDetectors(gcp.NewDetector()),
+		// Keep the default detectors
+		resource.WithTelemetrySDK(),
+		// Add your own custom attributes to identify your application
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("my-application"),
+			semconv.ServiceNamespaceKey.String("my-company-frontend-team"),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// For the demonstration, use sdktrace.AlwaysSample sampler to sample all traces.
+	// In a production application, use sdktrace.ProbabilitySampler with a desired probability.
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(
+		propagation.NewCompositeTextMapPropagator(
+			// Putting the CloudTraceOneWayPropagator first means the TraceContext propagator
+			// takes precedence if both the traceparent and the XCTC headers exist.
+			gcppropagator.CloudTraceOneWayPropagator{},
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		))
+	return tp, nil
 }
 
 // [END functions_helloworld_get]
